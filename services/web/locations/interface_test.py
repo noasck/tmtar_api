3423abaa23e27bd067cc869592f00d8a9175d7ca
noasck <de_te@mail.ru>
2020-07-23 14:42:06 +0300

    tested location entity

---
+++ services/web/locations/interface_test.py
@@ -0,0 +1,17 @@
+from pytest import fixture
+from .model import Location
+from .interface import ILocation
+
+
+@fixture
+def interface() -> ILocation:
+    return ILocation(id=1, name="test city", root=0)
+
+
+def test_ILocation_create(interface: ILocation):
+    assert interface
+
+
+def test_ILocation_works(interface: ILocation):
+    location = Location(**interface)
+    assert location
--- services/web/tests/fixtures.py
+++ services/web/tests/fixtures.py
@@ -1,16 +1,15 @@
 import pytest
 
-from ..project import app as imported_app
+from services.web.project import app, db
 
 @pytest.fixture
 def app():
-    return imported_app
+    return app
 
 @pytest.fixture
 def db(app):
-    from ..project import db
     with app.app_context():
-        db.create_all()
-        yield db
         db.drop_all()
+        db.create_all()
         db.session.commit()
+        yield db

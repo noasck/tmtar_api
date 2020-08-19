0ca36817630b4c592216b036476248e1ca651716
noasck <de_te@mail.ru>
2020-08-19 14:51:39 +0300

    some other headers added

--- services/web/tmtar/commands/cors_headers.py
+++ services/web/tmtar/commands/cors_headers.py
@@ -6,4 +6,5 @@ app = FlaskApp.Instance().app
 @app.after_request
 def after_request(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
     return response

df000d2af2c760698697d9a5b3947a1858f7c0fa
Hapon Maksym <max.gapon2001@gmail.com>
2020-11-28 18:52:43 +0200

    added object contreller test

--- services/web/tmtar/objects/controller_test.py
+++ services/web/tmtar/objects/controller_test.py
@@ -1,149 +1,130 @@
-# from unittest.mock import patch
-# from flask.testing import FlaskClient
-# from ..tests.fixtures import client, app, token # noqa
-# from .service import UserService
-# from pytest import fixture # noqa
-# from .model import User
-# from .schema import UserSchema
-# from .interface import IUser
-# from . import BASE_ROUTE
-# from ..project.types import *
-# from flask_jwt_extended import get_jti
-# from random import random
-# from datetime import datetime
-#
-#
-# def make_common_user(email: str, id=int(1000*random())) -> User: # noqa
-#     return User(id=id, email_hash=str(hash(email)), sex=SexType[0],
-#                 bdate=datetime.now().date(), location_id=1, role=RoleType[0])
-#
-#
-# def make_root_user() -> User:
-#     return User(id=1, email_hash=str(hash("some_str_admin")), sex=SexType[0],
-#                 bdate=datetime.now().date(), location_id=1, role=RoleType[2])
-#
-#
-# def make_update(usr: User, usr_upd: IUser) -> User:
-#     usr.update(usr_upd)
-#     return usr
-#
-#
-# @patch.object(
-#     UserService,
-#     "get_or_new_by_email",
-#     lambda email: make_root_user()
-# )
-# def create_token(client: FlaskClient):
-#     with client:
-#         result = client.get(f"/api/{BASE_ROUTE}/login/some_str_admin@mail.com").get_json()
-#         return result['access_token']
-#
-#
-# class TestUserLoginResource:
-#     @patch.object(
-#         UserService,
-#         "get_or_new_by_email",
-#         lambda email: make_common_user(email)
-#     )
-#     def test_token(self, client: FlaskClient):
-#         with client:
-#             result = client.get(f"/api/{BASE_ROUTE}/login/new-email@mail.com").get_json()
-#             assert get_jti(result['access_token'])
-#
-#
-# class TestUserResource:
-#     @patch.object(
-#         UserService,
-#         "get_all",
-#         lambda: [make_root_user(), make_common_user("sample-1@email.com"), make_common_user("sample-2@email.com")]
-#     )
-#     def test_get(self, client: FlaskClient, token: str):
-#         with client:
-#             result = client.get(f"/api/{BASE_ROUTE}/", headers={"Authorization": f"Bearer {token}"},
-#                                 follow_redirects=True).get_json()
-#             expected = UserSchema(many=True).dump(
-#                 [
-#                     make_root_user(), make_common_user("sample-1@email.com"), make_common_user("sample-2@email.com")
-#                 ]
-#             )
-#             print(result)
-#             for i in result:
-#                 assert i in expected
-#
-#     @patch.object(
-#         UserService,
-#         "get_by_id",
-#         lambda id: make_root_user() # noqa
-#     )
-#     @patch.object(
-#         UserService,
-#         "update",
-#         lambda usr, upd: make_update(usr, upd)
-#     )
-#     def test_put(self, client: FlaskClient, token):
-#         with client:
-#             result = client.put(
-#                 f"/api/{BASE_ROUTE}/",
-#                 headers={"Authorization": f"Bearer {token}"},
-#                 json={
-#                     "location_id": 2,
-#                     "bdate": '2018-03-09'
-#                 }
-#             ).get_json()
-#
-#             expected = UserSchema().dump(
-#                 User(id=1, email_hash=str(hash("some_str_admin")), sex=SexType[0],
-#                      bdate=datetime.strptime('2018-03-09', '%Y-%m-%d').date(), location_id=2, role=RoleType[2])
-#             )
-#
-#             assert result == expected
-#
-#
-# class TestUserIdResource:
-#     @patch.object(UserService, "get_by_id",
-#                   lambda user_id:
-#                   make_common_user(email='str1', id=user_id),
-#                   )
-#     def test_get(self, client: FlaskClient, token: str):
-#         with client:
-#             result = client.get(f"/api/{BASE_ROUTE}/13", headers={"Authorization": f"Bearer {token}"},
-#                                 follow_redirects=True).get_json()
-#             expected = (
-#                 UserSchema()
-#                 .dump(
-#                     make_common_user(email='str1', id=13),
-#                 )
-#             )
-#             assert result == expected
-#
-#     @patch.object(UserService, "delete_by_id", lambda user_id: user_id)
-#     def test_delete(self, client: FlaskClient, token: str):
-#         with client:
-#             result = client.delete(f"/api/{BASE_ROUTE}/13", headers={"Authorization": f"Bearer {token}"},
-#                                    follow_redirects=True).get_json()
-#             expected = dict(status="Success", id=13)
-#             assert result == expected
-#
-#     @patch.object(UserService, "get_by_id", lambda user_id: make_common_user(email='string', id=user_id))
-#     @patch.object(UserService, "update", make_update)
-#     def test_put(self, client: FlaskClient, token: str):
-#         with client:
-#             result = client.put(
-#                 f"/api/{BASE_ROUTE}/13",
-#                 headers={"Authorization": f"Bearer {token}"},
-#                 follow_redirects=True,
-#                 json={
-#                     "location_id": 2,
-#                     "bdate": '2018-03-09'
-#                 }
-#             ).get_json()
-#
-#             expected = (
-#                 UserSchema()
-#                 .dump(
-#                     User(id=13, email_hash=str(hash("string")), sex=SexType[0],
-#                          bdate=datetime.strptime('2018-03-09', '%Y-%m-%d').date(), location_id=2,
-#                          role=RoleType[0])
-#                 )
-#             )
-#             assert result == expected
+from unittest.mock import patch
+from flask.testing import FlaskClient
+
+from ..tests.fixtures import *  # noqa
+from .service import ObjectService
+from .model import Object
+from .schema import ObjectSchema
+from .interface import IObject
+from . import BASE_ROUTE
+
+
+def make_object(
+        object_id: int = 12, name: str = 'Test object', target_image_file="some image",
+        asset_file="asset file", subzone_id: int = 0
+) -> Object:
+    return Object(id=object_id, name=name, target_image_file=target_image_file,
+                  asset_file=asset_file, subzone_id=subzone_id)
+
+
+def make_update(obj: Object, obj_upd: IObject) -> Object:
+    new_object = make_object(obj.id, obj_upd['name'], obj_upd['target_image_file'],
+                             obj_upd['asset_file'], obj_upd['subzone_id'])
+    return new_object
+
+
+class TestObjectResource:
+    @patch.object(
+        ObjectService,
+        "get_all",
+        lambda: [
+            make_object(3, 'Test object 1', "Image 1", "Asset file 5", 2),
+            make_object(object_id=7, name='Test 2', target_image_file="Image 2")
+        ]
+    )
+    def test_get(self, client: FlaskClient):
+        with client:
+            result = client.get(f"/api/{BASE_ROUTE}", follow_redirects=True).get_json()
+            expected = (
+                ObjectSchema(many=True)
+                .dump(
+                    [
+                        make_object(3, 'Test object 1', "Image 1", "Asset file 5", 2),
+                        make_object(object_id=7, name='Test 2', target_image_file="Image 2")
+                    ]
+                )
+            )
+            assert len(result) != 0
+            for i in result:
+                assert i in expected
+
+    @patch.object(
+        ObjectService, "create", lambda create_request: Object(**create_request)
+    )
+    def test_post(self, client: FlaskClient):
+        with client:
+            payload = dict(name="Sample Object")
+            result = client.post(f"/api/{BASE_ROUTE}", json=payload).get_json()
+            expected = (
+                ObjectSchema().dump(
+                    Object(name=payload['name'])
+                )
+            )
+            assert result == expected
+
+
+class TestObjectIdResource:
+    @patch.object(ObjectService, "get_by_id",
+                  lambda object_id: make_object(object_id, name="Test object 1",
+                                                target_image_file="Image 1",
+                                                subzone_id=4))
+    def test_get(self, client: FlaskClient):
+        with client:
+            result = client.get(f"/api/{BASE_ROUTE}/7")
+            expected = (
+                ObjectSchema()
+                .dump(
+                    make_object(object_id=7, name='Test 2', target_image_file="Image 2")
+                )
+            )
+            assert result in expected
+
+    @patch.object(ObjectService, "get_by_id", lambda object_id: make_object(object_id=object_id))
+    @patch.object(ObjectService, "update", make_update)
+    def test_put(self, client: FlaskClient):
+        with client:
+            result = client.put(
+                f"/api/{BASE_ROUTE}/7",
+                json={"name": "Test object 1", "object_id": 7, "target_image_file": "Image 1"}
+            ).get_json()
+
+            excepted = (
+                ObjectSchema()
+                .dump(make_object(object_id=7, name="Test object 1", target_image_file="Image 1"))
+            )
+            assert result == excepted
+
+    @patch.object(ObjectService, "delete_by_id",
+                  lambda object_id: object_id)
+    def test_delete(self, client: FlaskClient):
+        with client:
+            result = client.delete(f"/api/{BASE_ROUTE}/7").get_json()
+            excepted = dict(status="Success", id=7)
+        assert result == excepted
+
+
+class TestObjectSubzoneIdResource:
+    @patch.object(ObjectService, "get_by_subzone_id",
+                  lambda subzone_id: make_object(name="Test 1", subzone_id=3))
+    def test_get(self, client: FlaskClient):
+        with client:
+            result = client.get(f"/api/{BASE_ROUTE}/3")
+            expected = (
+                ObjectSchema(many=True).dump(
+                    [
+                        make_object(name='Test 1', subzone_id=3)
+                    ]
+                )
+            )
+            assert result in expected
+
+
+class TestObjectSearchResource:
+    @patch.object(ObjectService, "search_by_name",
+                  lambda str_to_find: [make_object(object_id=3, name=str_to_find)])
+    def test_get(self, client: FlaskClient):
+        result = client.get(f"/api/{BASE_ROUTE}/search/Object 2").get_json()
+        excepted_objects = ObjectSchema().dump([make_object(object_id=3, name="Object 2")], many=True)
+        excepted = dict(status="Match", objects=excepted_objects)
+
+        assert result == excepted

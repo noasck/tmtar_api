7d249c3c05232e82e2c37b0ba1d3eb201e8d6ff8
Dima <dimalgpf@ukr.net>
2021-04-26 18:38:18 +0300

    little fixes for flake8 check

--- services/web/tmtar/users/schema.py
+++ services/web/tmtar/users/schema.py
@@ -23,4 +23,5 @@ class UserInfoSchema(Schema):
 
 
 class UserAdminLocationIdSchema(Schema):
-    admin_location_id = fields.Number(attribute='admin_location_id', required=True)
+    admin_location_id = fields.Number(attribute='admin_location_id',
+                                      required=True)

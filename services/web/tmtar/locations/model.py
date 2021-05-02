f5933f01df804edb9f4738edb2a2b2fdc9957df5
noasck <denter425@gmail.com>
2021-05-03 00:22:59 +0300

    fix loation cascade deletion

--- services/web/tmtar/locations/model.py
+++ services/web/tmtar/locations/model.py
@@ -18,6 +18,8 @@ class Location(db.Model):
     children = db.relationship(
         'Location',
         backref=db.backref('parent', remote_side=[id]),
+        # cascade="all, delete-orphan",
+        passive_deletes=True,
     )
 
     def update(self, changes: ILocation):

1964626d8d29dfb444a7afea1a1e68c026387677
langelius <ogk.langelius@gmail.com>
2021-07-16 06:53:05 +0300

    files schema fix (made id field dump only and removed id from tests)

--- services/web/tmtar/files/schema.py
+++ services/web/tmtar/files/schema.py
@@ -2,5 +2,5 @@ from marshmallow import Schema, fields
 
 
 class FileSchema(Schema):
-    id = fields.Number(attribute='id')
+    id = fields.Number(attribute='id', dump_only=True)
     filename = fields.String(attribute='filename')

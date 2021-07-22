1fad1df4d5b0c823bbff53e824399c23016ecd9d
supruniiuk <juliasupruniuk23@gmail.com>
2021-07-22 16:05:05 +0300

    added mock db

---
+++ services/frontend/src/app/shared/mockDB/users.js
@@ -0,0 +1,20 @@
+let users = [
+	{
+		"admin_location_id": 1,
+		"sex": null,
+		"identity": "google-oauth2|112161506929078504169",
+		"id": 4,
+		"bdate": null,
+		"location_id": 2
+	},
+	{
+		"admin_location_id": 2,
+		"sex": null,
+		"identity": "auth0|609fd1ff3872bb0068e63812",
+		"id": 5,
+		"bdate": null,
+		"location_id": 5
+	}
+]
+
+export default users
